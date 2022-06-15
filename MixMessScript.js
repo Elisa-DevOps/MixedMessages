a2023b3c7dc55a7f5e0d0f746b3bcf6dab6563e9
Elisa <elisa-devops@users.noreply.github.com>
2022-06-15 16:55:19 -0500

    square ascii art added

--- MixMessScript.js
+++ MixMessScript.js
@@ -15,11 +15,25 @@ const returnRandNeed = () => {
     return needs[Math.floor(Math.random() * 3)]; //selects a random array value
   };
 
+  const returnAsciiArt = () => {
+    //let i = 0;
+    let arts1 = ['--------', '|      |', '--------'];
+    for (let i = 0; i < 3; i++) {
+    let arts2 = arts1[i];
+    //const arts2 = ['|      |'];
+    //const arts3 = ['--------'];
+    //return needs[Math.floor(Math.random() * 3)]; //selects a random array value
+    console.log(arts2);
+    }
+  };
   /////////////////script start///////////
   let need = returnRandNeed();
   let want = returnRandWant();
   let quote = returnRandQuote();
+  //let art = returnAsciiArt();
 
   console.log(`You Need: ${need}`); //outputs either sleep, food, water
   console.log(`You Want: ${want}`); //outputs either vacation, pool time, cheese
   console.log(`Remember: ${quote}`); //outputs either 'Be someone you look up to', 'You are enough', 'Set boundaries. No is an answer.'
+  console.log(`Ascii Art: `);
+  returnAsciiArt();
